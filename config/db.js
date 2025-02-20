956327b02f36f0ac90a2afc1aeae58b6652638b6
Deakon89 <franck.devw@gmail.com>
2025-02-20 12:08:24 +0100

    add readme

--- config/db.js
+++ config/db.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 
 
 const connection = mysql.createConnection({
-    // host:"localhost",
-    // user:"root",
-    // password:"",
-    // database:"db"
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
